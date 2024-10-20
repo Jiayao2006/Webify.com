9933e4fd4f21761b456b38f6ddd81dfd821b1fca
Jiayao2006 <148928521+Jiayao2006@users.noreply.github.com>
2024-10-21 00:55:07 +0800

    edited the deadline

--- JS/deadline.js
+++ JS/deadline.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("August 20, 2024 00:00:00").getTime(); // Advanced Web Development
+var countDownDate = new Date("August 20, 2030 00:00:00").getTime(); // Advanced Web Development
 
 // Update the count down every 1 second
 var x = setInterval(function() {
